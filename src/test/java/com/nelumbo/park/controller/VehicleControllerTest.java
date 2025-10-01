0c5502712429acc4b9ed06c64c9f20763ea5c05c
jesus cantor <jedacan58@gmail.com>
2025-10-01 10:03:27 -0500

    Edit VehicleControllerTest.java by Github Actions

--- src/test/java/com/nelumbo/park/controller/VehicleControllerTest.java
+++ src/test/java/com/nelumbo/park/controller/VehicleControllerTest.java
@@ -15,7 +15,8 @@ import com.nelumbo.park.repository.UserRepository;
 import com.nelumbo.park.service.VehicleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -32,7 +33,8 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(VehicleController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @Import(TestSecurityConfig.class)
 class VehicleControllerTest {
 
