5f36089fca5d1c94dcf3b5a311085a027050a8e6
jesus cantor <jedacan58@gmail.com>
2025-10-01 17:57:21 -0500

    Edit variable spring.jpa.hibernate.ddl-auto in application.properties

--- src/main/java/com/nelumbo/park/Application.java
+++ src/main/java/com/nelumbo/park/Application.java
@@ -8,12 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("--- DEBUGGING ENVIRONMENT VARIABLES ---");
-        System.out.println("SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
-        System.out.println("SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
-        System.out.println("SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
-        System.out.println("SPRING_DATASOURCE_DRIVER_CLASS_NAME: " + System.getenv("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
-        System.out.println("------------------------------------");
         SpringApplication.run(Application.class, args);
     }
 }
