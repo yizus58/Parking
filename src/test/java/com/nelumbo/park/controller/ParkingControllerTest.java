2e3487a830b48939b146666a3aaf9ba3d1c03d83
jesus cantor <jedacan58@gmail.com>
2025-10-02 17:32:43 -0500

    Edit the file TestSecurityConfig.java by debugging erros in ParkingControllerTest

--- src/test/java/com/nelumbo/park/controller/ParkingControllerTest.java
+++ src/test/java/com/nelumbo/park/controller/ParkingControllerTest.java
@@ -12,6 +12,7 @@ import com.nelumbo.park.repository.UserRepository;
 import com.nelumbo.park.service.ParkingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -27,7 +28,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(ParkingController.class)
+@WebMvcTest(value = ParkingController.class, excludeAutoConfiguration = UserDetailsServiceAutoConfiguration.class)
 class ParkingControllerTest {
 
     @Autowired
