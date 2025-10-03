c8b8a328ba6e1bb40741161a5a91d1bc36c3fa41
jesus cantor <jedacan58@gmail.com>
2025-10-03 15:41:50 -0500

    Remove permits

--- src/main/java/com/nelumbo/park/config/security/SecurityConfig.java
+++ src/main/java/com/nelumbo/park/config/security/SecurityConfig.java
@@ -35,8 +35,6 @@ public class SecurityConfig {
                     .requestMatchers("/auth/**").permitAll()
                     .requestMatchers("/v3/**").permitAll()
                     .requestMatchers("/swagger-ui/**").permitAll()
-                    .requestMatchers("/api/sonarqube-webhook").permitAll()
-                    .requestMatchers("/api/sonarqube-logs").permitAll()
                     .anyRequest().authenticated()
             )
             .exceptionHandling(exceptions -> exceptions
