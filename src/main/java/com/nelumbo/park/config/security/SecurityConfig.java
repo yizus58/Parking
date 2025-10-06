82f8343e4c4cdc9dacf368ba973e32813496c42e
jesus cantor <jedacan58@gmail.com>
2025-10-06 09:19:44 -0500

    Fixed error report by Sonarqube

--- src/main/java/com/nelumbo/park/config/security/SecurityConfig.java
+++ src/main/java/com/nelumbo/park/config/security/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authz -> authz
                     .requestMatchers("/auth/**").permitAll()
