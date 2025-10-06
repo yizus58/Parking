1f41f7b82fba0bd350c1607777277c203f2f902f
jesus cantor <jedacan58@gmail.com>
2025-10-06 10:00:55 -0500

    Fixed error NoOpPasswordEncoder

--- src/test/java/com/nelumbo/park/config/TestSecurityConfig.java
+++ src/test/java/com/nelumbo/park/config/TestSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -76,6 +76,6 @@ public class TestSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
